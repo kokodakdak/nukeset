e5b3a3cdd31039d5d7fde1fb654306f6e9808224
kokodakdak <kokodak95@naver.com>
2019-02-18 09:20:30 +0900

    이미지추가

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 import nuke
 
 tb = nuke.toolbar("Nodes")
-m = tb.addMenu("Kokodak")
+m = tb.addMenu("Kokodak", icon="kokodak_logo.png")
 m.addMenu("Draw")
 m.addCommand("Draw/Timecode","nuke.createNode('burnintc')")
