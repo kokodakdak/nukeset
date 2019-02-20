53a87e351b6848c4b0a4f0fd3883f5607fb91367
kokodakdak <kokodak95@naver.com>
2019-02-20 10:45:48 +0900

    pyside를 이용하여 리펙토링

--- scripts/checkenv.py
+++ scripts/checkenv.py
@@ -1,8 +1,46 @@
 #coding:utf8
 import os
-import nuke
-#from PySide2.QtWidgets import *
+#import nuke
+from PySide2.QtWidgets import *
 
+
+
+"""
+뉴크 의존성도 제거할수 있다. 
+"""
+class CheckEnv(QWidget):
+	layout = QVBoxLayout()
+	def __init__(self):
+		super(CheckEnv, self).__init__()
+		self.setLayout(self.layout)
+		self.setEnv()
+
+	def setEnv(self):
+		envs = ["USER", "OCIO", "NUKE_PATH","NUKE_FONT_PATH"]
+		for e in envs:
+			self.layout.addWidget(QLabel("$%s : %s" % (e,os.environ[e])))
+
+def main():
+	global customApp
+	try:
+		customApp.close()
+	except:
+		pass
+	customApp = CheckEnv()
+	try:
+		customApp.show()
+	except:
+		pass
+
+
+
+
+
+
+
+
+"""
+큐티위젯으로 짜기 전
 def main():
 
 	results = []
@@ -11,4 +49,4 @@ def main():
 	for e in envs:
 		results.append("$%s : %s" % (e, os.environ[e]))
 	nuke.message("\n".join(results))
-
+"""
